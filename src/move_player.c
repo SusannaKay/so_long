5682f100a4a2ac76bc5143dd6b303785b78094b8
Susanna Kayed <skayed@e3r2p6.42roma.it>
2025-03-10 18:01:35 +0100

    Draft: move player logic

--- src/move_player.c
+++ src/move_player.c
@@ -1,8 +1,17 @@
-#include "so_long.h"
+#include "../so_long.h"
 
-int move_player(int keysym, t_map *map)
+int move_player(int keycode, t_map *map)
 {
-    if (keysym == e)
-        exit_game(map, "Quit program");
-    return (0);
+    if (keycode == UP)
+        // se non e un muro x-1, y
+        // prendi vecchi x e y, trasforma valore in 0
+        // nuovi valori, se e' C aumenta score +1 , se e' EXIT se score = collezionabili, end game, trasforma valore in P 
+    if (keycode == DOWN)
+        // se non e un muro x+1, y
+    if (keycode == LEFT)
+        // se non e un muro x, y - 1
+    if (keycode == RIGHT)
+        // se non e un muro x, y + 1
+    
+    // render map
 }
