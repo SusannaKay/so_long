a2f05a4bad64b9c8d07be943b1d485eac7673a5e
Susanna Kayed <skayed@e3r2p6.42roma.it>
2025-03-10 18:06:32 +0100

    Add: rendering function

--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
-#include "so_long.h"
+#include "../so_long.h"
 
 static void create_window(t_map *map)
 {
     map->mlx = mlx_init();
     if (!map->mlx)
         free(map);
-    map->win = mlx_new_window(map->mlx, TILESIZE * map->rows, TILESIZE * map->len_row, "So Long");
+    map->win = mlx_new_window(map->mlx, (TILESIZE * map->len_row), (TILESIZE * map->rows), "So Long");
     if (!map->win)
         exit_game(map, "Finestra non creata");
 }
@@ -17,7 +17,7 @@ static int close_win(int keysym, t_map *map)
 }
 static void create_loop(t_map *map)
 {
-    mlx_hook(map->win, KeyPress, KeyPressMask, move_player, map);
+    //mlx_hook(map->win, KeyPress, KeyPressMask, move_player, map);
     mlx_hook(map->win, KeyPress, KeyPressMask, close_win, map);
     mlx_hook(map->win, 17, 0L, exit_game, map);
     mlx_loop(map->mlx);
@@ -40,7 +40,11 @@ int main(int argc, char *argv[])
         verify_map(map);
         graphics = create_tgraphics(graphics);
         map->graphics = graphics;
+        if(!map->graphics)
+            {ft_printf("graphics e' null");
+            return(0);}
         create_window(map);
+        render_map(map);
         create_loop(map);
         return (0);
     }
