399684592ca03adf3a55e5fcea2a4f3b34db0b3a
Susanna kayed <kefka.sk@gmail.com>
2025-03-11 10:36:47 +0100

    Add: developed move function

--- src/move_player.c
+++ src/move_player.c
@@ -2,16 +2,31 @@
 
 int move_player(int keycode, t_map *map)
 {
+    int new_x;
+    int new_y;
+
+    new_x = map->p_x;
+    new_y = map->p_y;
     if (keycode == UP)
-        // se non e un muro x-1, y
-        // prendi vecchi x e y, trasforma valore in 0
-        // nuovi valori, se e' C aumenta score +1 , se e' EXIT se score = collezionabili, end game, trasforma valore in P 
+        new_x -= 1;
     if (keycode == DOWN)
-        // se non e un muro x+1, y
+        new_x += 1;
     if (keycode == LEFT)
-        // se non e un muro x, y - 1
+        new_y -= 1;
     if (keycode == RIGHT)
-        // se non e un muro x, y + 1
-    
-    // render map
+        new_y += 1;
+    if (map->map[new_y][new_x] == '1')
+        return (1);
+    map->map[map->p_y][map->p_x] = '0';
+    if (map->map[new_y][new_x] == 'C')
+        map->score++;
+    if (map->map[new_y][new_x] == 'E' && map->score == map->collect)
+        return (exit_game(map, "You win!\n"), 0);
+    map->map[new_y][new_x] = 'P';
+    map->moves++;
+    ft_printf("moves number: %d", map->moves);
+    map->p_x = new_x;
+    map->p_y = new_y;
+    render_map(map);
+    return (1);
 }
