b3d754ada6ad9f893dcca91e42bed5b3eb9ac0b2
Susanna <kefka.sk@gmail.com>
2025-03-14 13:24:56 +0100

    Chore: split main function in two

--- src/move_player.c
+++ src/move_player.c
@@ -6,15 +6,39 @@
 /*   By: skayed <skayed@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 10:32:59 by skayed            #+#    #+#             */
-/*   Updated: 2025/03/13 21:12:01 by skayed           ###   ########.fr       */
+/*   Updated: 2025/03/14 13:23:34 by skayed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
-static int check_moves(t_map    *map, int new_x, int new_y)
+void update_position(int keysym, t_map *map, int *new_x, int *new_y)
 {
+    *new_x = map->p_x;
+    *new_y = map->p_y;
+
+    if (keysym == UP)
+        *new_y -= 1;
+    if (keysym == DOWN)
+        *new_y += 1;
+    if (keysym == LEFT)
+        *new_x -= 1;
+    if (keysym == RIGHT)
+        *new_x += 1;
+
+}
+
+int move_player(int keysym, t_map *map)
+{
+    int new_x;
+    int new_y;
+    
+    if (keysym != UP && keysym != DOWN && keysym != LEFT && keysym != RIGHT)
+        return (1);
+    update_position(keysym, map, &new_x, &new_y);
     if (map->map[new_y][new_x] == 'E' && map->score == map->collect)
-            return (exit_game(map, "You win!\n"), 0);
+        return (exit_game(map, "You win!\n"), 0);
+    if (map->map[new_y][new_x] == 'A')
+        return (exit_game(map, "You Loose!\n"), 0);
     else if (map->map[new_y][new_x] == 'E' && map->score != map->collect)
         ft_printf("You must collect all the cookies!\n");
     if (map->map[new_y][new_x] != '1' && map->map[new_y][new_x] != 'E')  
@@ -24,34 +48,10 @@ static int check_moves(t_map    *map, int new_x, int new_y)
             map->score++;
         map->map[new_y][new_x] = 'P';
         map->moves++;
-    } 
-    return(1);
-}
-
-int move_player(int keysym, t_map *map)
-{
-    int new_x;
-    int new_y;
-
-    new_x = map->p_x;
-    new_y = map->p_y;
-    if (keysym == UP || keysym == DOWN || keysym == LEFT || keysym == RIGHT)
-    {
-        if (keysym == UP)
-            new_y -= 1;
-        if (keysym == DOWN)
-            new_y += 1;
-        if (keysym == LEFT)
-            new_x -= 1;
-        if (keysym == RIGHT)
-            new_x += 1;
-        if (check_moves(map, new_x, new_y))
-        {
-            ft_printf("Moves number: %d\n", map->moves);
-            map->p_x = new_x;
-            map->p_y = new_y;
-            return (render_map(map, 1), 0);
-        }
+        ft_printf("Moves number: %d\n", map->moves);
+        map->p_x = new_x;
+        map->p_y = new_y;
+        return (render_map(map, 1), 0);
     }
     return (1);
 }
