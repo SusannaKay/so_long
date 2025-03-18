953991593f2fbe1e99795414acc0b63c95ee894b
Susanna <kefka.sk@gmail.com>
2025-03-18 23:22:37 +0100

    Fix: enemy movement erasing collectibles and exit

--- bonus/bonus_enemy.c
+++ bonus/bonus_enemy.c
@@ -10,13 +10,16 @@ int enemy_patrol(t_map *map)
         return(0);
     map->enemy->frame_delay = 0;
     next_x = map->enemy->x + map->enemy->direction;
-    if (map->map[map->enemy->y][next_x] == '1') 
+    if (map->map[map->enemy->y][next_x] == '1' || map->map[map->enemy->y][next_x] == 'E' || map->map[map->enemy->y][next_x] == 'A' || map->map[map->enemy->y][next_x] == 'C') 
         map->enemy->direction *= -1; 
     else
     {
-        map->map[map->enemy->y][map->enemy->x] = '0'; 
+        if(map->map[map->enemy->y][next_x] == 'P')
+            return (exit_game(map, "You Loose!\n"), 0);
+        if(map->map[map->enemy->y][next_x] != 'C')
+        {map->map[map->enemy->y][map->enemy->x] = '0'; 
         map->enemy->x = next_x;                      
-        map->map[map->enemy->y][map->enemy->x] = 'A'; 
+        map->map[map->enemy->y][map->enemy->x] = 'A'; }
     }
     return (0);
 }
